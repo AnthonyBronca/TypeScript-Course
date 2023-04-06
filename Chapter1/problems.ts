a43fcd8118657ed6433a8e7fa6d1e51d798d3908
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-04-06 18:59:16 -0400

    added more problems

--- Chapter1/problems.ts
+++ Chapter1/problems.ts
@@ -1,3 +1,6 @@
+//Drag the test cases down to test your problems
+
+
 /*
 
 Write a function calculateArea that calculates the area of a rectangle.
@@ -16,3 +19,31 @@ calculateArea('10',3) // should show an error around '10' before tsc
 calculateArea()//should show an error under calculateArea before tsc
 
 */
+
+
+//Your code here:
+
+
+
+
+
+/*
+
+Write a function reverseArray that takes an array of strings
+ as input and returns a new array with the same elements
+ in reverse order.
+
+Declare the type of the function parameter and return value.
+
+
+reverseArray(['f','o','o']) // ['o','o','f']
+reverseArray(['f','i','z','z']) // ['z','z','i', 'f]
+
+//Comment these out before running tsc
+reverseArray([1,2,3,4]) // should show error for each element since they are numbers
+
+
+*/
+
+
+//Your code here:
--- Chapter1/solutions.ts
+++ Chapter1/solutions.ts
@@ -29,3 +29,68 @@ calculateArea(10,3) // 30
 //Comment these out before running tsc
 // calculateArea('10',3) // should show an error around '10' before tsc
 // calculateArea()//should show an error under calculateArea before tsc
+
+
+
+/*
+
+Write a function reverseArray that takes an array of strings
+ as input and returns a new array with the same elements
+ in reverse order.
+
+Declare the type of the function parameter and return value.
+
+
+reverseArray(['f','o','o']) // ['o','o','f']
+reverseArray(['f','i','z','z']) // ['z','z','i', 'f]
+
+//Comment these out before running tsc
+reverseArray([1,2,3,4]) // should show error for each element since they are numbers
+
+
+
+
+*/
+
+
+function reverseArray(arr: string[]): string[]{
+
+    const reverseArr: string[] = arr.reverse();
+    return reverseArr;
+}
+
+reverseArray(['f','o','o']) // ['o','o','f']
+reverseArray(['f','i','z','z']) // ['z','z','i', 'f]
+
+//reverseArray([1,2,3,4]) // should show error for each element since they are numbers
+
+
+/*
+
+Write a function printId that takes an argument that can be
+ either a string or a number and prints it to the console.
+ The function should return the string.
+
+const strId: string = 'abc123';
+const numId: number = 456;
+printId(strId); // prints "ID: abc123"
+printId(numId); // prints "ID: 456"
+
+//Comment these out before running tsc
+printId(); // shows error under printId
+*/
+
+function printId(id: string | number): string{
+    let strId: string = `ID: ${id}`
+    console.log(strId);
+    return strId;
+
+}
+
+const strId: string = 'abc123';
+const numId: number = 456;
+printId(strId); // prints "ID: abc123"
+printId(numId); // prints "ID: 456"
+
+//Comment these out before running tsc
+// printId(); // shows error under printId
