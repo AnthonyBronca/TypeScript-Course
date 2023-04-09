a75e1b00d219ef83fc2fd04319948312719aba1d
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-04-09 19:36:20 -0400

    updated tsconfig.json

--- Chapter3/dom.js
+++ Chapter3/dom.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
     console.log('hello');
     console.log('hi');
+    console.log('test');
 });
--- Chapter3/dom.ts
+++ Chapter3/dom.ts
@@ -3,8 +3,8 @@ window.addEventListener("DOMContentLoaded", ()=> {
 
     console.log('hello')
     console.log('hi')
-    console.log('hi')
-
+    console.log('test')
+    console.log('hello?')
 
 
 
